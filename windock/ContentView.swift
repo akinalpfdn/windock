ac32bbb0c5cd358de10057c66d5e05de005e3800
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2025-12-02 00:11:25 +0300

    Initial Commit

---
+++ windock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  windock
+//
+//  Created by Akinalp Fidan on 2.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ windock/windockApp.swift
@@ -0,0 +1,17 @@
+//
+//  windockApp.swift
+//  windock
+//
+//  Created by Akinalp Fidan on 2.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct windockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
